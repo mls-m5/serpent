e5b51f3f75126d8502db8f318a0b16cd4a3a05f0
Hannes Kindströmmer <hannes@kindstrommer.se>
2021-11-05 17:15:37 +0100

    Move to webpack

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import {cool} from './test';
+
 const canvas = document.getElementById('canvas');
 const test = document.getElementById('hello');
 
 if(test) test.innerText = 'COool';
 
+cool();
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export function cool(){
+    alert('Hi');
+}
