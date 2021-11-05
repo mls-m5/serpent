279bc87eb59d973e26f2f28600242a1d782165f7
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-11-05 22:44:40 +0100

    Playable state

---
+++ src/obstacle.ts
@@ -0,0 +1,26 @@
+
+import { Gfx } from "./gfx";
+import { Vec2 } from "./vec2";
+
+export class Obstacle {
+    public isDead = false;
+
+    constructor(private pos: Vec2, private size: number, private gfx: Gfx) { }
+
+    isCollision(v: Vec2, size: number): boolean {
+        let dx = this.pos.x - v.x;
+        let dy = this.pos.y - v.y;
+
+        let d = this.size + size;
+        return dx * dx + dy * dy < d * d;
+    }
+
+    draw() {
+        this.gfx.drawBodySegment({ x: this.pos.x, y: this.pos.y, angle: 0 });
+    }
+
+    update() {
+        // Todo check if outside 
+    }
+
+}
