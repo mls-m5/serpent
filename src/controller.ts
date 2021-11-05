4d497a4659cf7b92c88b7b1bf8275f6a89bfe0f2
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-11-05 22:44:31 +0100

    Playable state

--- src/controller.ts
+++ src/controller.ts
@@ -1,13 +1,24 @@
 import { KeyboardKey } from "./keyboard-key";
 
+type callback = () => any;
+
 export class Controller {
     private keyInputs = new Set<KeyboardKey>();
+    private callbacks = new Map<KeyboardKey, callback[]>();
+
     constructor(document: Document) {
         document.addEventListener('keydown', event => this.keyDownHandler(event), false);
         document.addEventListener('keyup', event => this.keyUpHandler(event), false);
     }
     public isKeyPressed(key: KeyboardKey): boolean {
         return this.keyInputs.has(key);
+    }
+
+    public onKeyPressed(key: KeyboardKey, callback: callback) {
+        if (!this.callbacks.has(key)) {
+            this.callbacks.set(key, []);
+        }
+        this.callbacks.get(key)?.push(callback);
     }
 
     private keyDownHandler(event: { keyCode: KeyboardKey }) {
--- src/game.ts
+++ src/game.ts
@@ -3,6 +3,7 @@ import { Gfx } from "./gfx";
 import { Serpent } from "./serpent";
 import { Vec2 } from "./vec2";
 import { Controller } from './controller';
+import { KeyboardKey } from './keyboard-key';
 import { GameAudio } from "./game-audio";
 
 
@@ -11,16 +12,24 @@ export class Game {
     private controller: Controller;
 
     private scrollAmount = 0;
+    private runGameLoop = true;
 
     constructor(
         private gfx: Gfx,
         private audio: GameAudio
     ) {
         this.controller = new Controller(document);
+        this.controller.onKeyPressed(KeyboardKey.SPACE, () => this.runGameLoop ? this.pause() : this.start());
+
         this.player = new Serpent({ x: 50, y: 100 }, this.controller, this.audio);
     }
 
+    public pause() {
+        this.runGameLoop = false;
+    }
+
     public start() {
+        this.runGameLoop = true;
         setTimeout(() => this.update(), 100);
     }
 
@@ -31,6 +40,9 @@ export class Game {
     }
 
     public update() {
+        if (!this.runGameLoop) {
+            return;
+        }
         this.scrollAmount += .1;
         this.draw();
 
