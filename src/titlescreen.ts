99a574d6b99e7034935ef0a6844b2481ae34f211
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-11-06 12:16:53 +0100

    title screen alignment

--- src/titlescreen.ts
+++ src/titlescreen.ts
@@ -4,11 +4,12 @@ import { Gfx } from "./gfx";
 export class TitleScreen {
 
     public draw(gfx: Gfx) {
-        gfx.drawText(500, 300, "A simple serpent", 150, true);
-        gfx.drawText(550, 350, "A game by", 20);
-        gfx.drawText(600, 380, "Mattias Larsson Sköld &", 20);
-        gfx.drawText(650, 410, "Hannes Kindströmmer", 20);
+        let x = -200;
+        gfx.drawText(500 + x, 300, "A simple serpent", 150, true);
+        gfx.drawText(550 + x, 350, "A game by", 20);
+        gfx.drawText(600 + x, 380, "Mattias Larsson Sköld &", 20);
+        gfx.drawText(650 + x, 410, "Hannes Kindströmmer", 20);
 
-        gfx.drawText(800, 800, "Press space to play", 60);
+        gfx.drawText(700, 800, "Press space to play", 60);
     }
 }
