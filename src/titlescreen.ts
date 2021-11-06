77c4bf072618e62f63cadc0888ae7350de82fddc
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-11-06 12:31:34 +0100

    title images

--- src/titlescreen.ts
+++ src/titlescreen.ts
@@ -11,5 +11,16 @@ export class TitleScreen {
         gfx.drawText(650 + x, 410, "Hannes Kindströmmer", 20);
 
         gfx.drawText(700, 800, "Press space to play", 60);
+
+        setTimeout(() => {
+            gfx.drawSprite({ x: 200, y: 100, angle: 0 }, gfx.sprites.energyDrink);
+            gfx.drawSprite({ x: 20, y: 100, angle: 0 }, gfx.sprites.orange);
+            gfx.drawSprite({ x: 30, y: 110, angle: 0 }, gfx.sprites.cherry);
+            gfx.drawSprite({ x: 40, y: 100, angle: 0 }, gfx.sprites.apple);
+
+            gfx.drawSprite({ x: 100, y: 120, angle: 0 }, gfx.sprites.rock1);
+            gfx.drawSprite({ x: 130, y: 120, angle: 0 }, gfx.sprites.rock3);
+            gfx.drawSprite({ x: 146, y: 120, angle: 0 }, gfx.sprites.rock2);
+        }, 400);
     }
 }
