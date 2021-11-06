2be9e07aa13e16d4db7de8dcf4380f441b1c7bd7
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-11-06 11:55:10 +0100

    sound loading

--- src/game-audio.ts
+++ src/game-audio.ts
@@ -2,12 +2,21 @@
 export class GameAudio {
     public Slither: HTMLAudioElement;
     public Swallow: HTMLAudioElement;
+    public Play: HTMLAudioElement;
+    public Energy: HTMLAudioElement;
+    public Died: HTMLAudioElement;
+
+    private loadSound(src: string): HTMLAudioElement {
+        let element = document.createElement("audio");
+        element.src = src;
+        return element;
+    }
 
     constructor(document: Document) {
-        this.Slither = document.createElement("audio");
-        this.Slither.src = "/assets/slither.wav";
-
-        this.Swallow = document.createElement("audio");
-        this.Swallow.src = "/assets/swallow.wav";
+        this.Slither = this.loadSound("/assets/slither.wav");
+        this.Swallow = this.loadSound("/assets/swallow.wav");
+        this.Play = this.loadSound("/assets/sfx/play.mp3");
+        this.Energy = this.loadSound("/assets/energy.mp3");
+        this.Died = this.loadSound("/assets/died.mp3");
     }
 }
